0d3e7d6eb17d224a65a85fce50c46cbbf618c9a4
Mariia Khapun <mariakhapun@gmail.com>
2023-03-14 15:27:53 +0000

    HW commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,73 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { Statistics } from './Statistics';
+import { FeedbackOptions } from './FeedbackOptions';
+import { Section } from './Section';
+import { Notification } from './Notification';
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleClick = event => {
+    const value = event.target.textContent.toLowerCase();
+
+    this.setState(prevState => {
+      switch (value) {
+        case 'good':
+          return { good: prevState.good + 1 };
+        case 'neutral':
+          return { neutral: prevState.neutral + 1 };
+        case 'bad':
+          return { bad: prevState.bad + 1 };
+        default:
+      }
+    });
+  };
+
+  countTotalFeedback() {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  }
+
+  countPositiveFeedbackPercentage() {
+    return Math.round((this.state.good / this.countTotalFeedback()) * 100);
+  }
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const positivePercentage = isNaN(this.countPositiveFeedbackPercentage())
+      ? 0
+      : this.countPositiveFeedbackPercentage();
+
+    const total = this.countTotalFeedback();
+
+    return (
+      <>
+        <Section title="Please leave your feedback">
+          <FeedbackOptions
+            options={['good', 'neutral', 'bad']}
+            onLeaveFeedback={this.handleClick}
+          ></FeedbackOptions>
+        </Section>
+        <Section title="Statistics">
+          {total > 0 ? (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={total}
+              positivePercentage={positivePercentage}
+            ></Statistics>
+          ) : (
+            <Notification message="No feedback given yet 😿"></Notification>
+          )}
+        </Section>
+      </>
+    );
+  }
+}
+
+export default App;
